6043aea6a0e7ad33eeb4b4b7863d0fd3c33bdbce
cdsalty <soltiscd@gmail.com>
2019-03-30 12:18:35 -0400

    created random colors and game responds accordingly

--- colorGame.js
+++ colorGame.js
@@ -1,77 +1,63 @@
-// alert("B00");
-/*
 
-We first need to setup javascript to make each one of our boxes is a diferrent color. 
-   1. We just want to start with assigning all the boxes a color. We start by creating each colors.
-        - select all six of the squares and loop through them and assign them one of the rgb options for the variable, "color"
+// Original list of colors assigned until the function to generate random colors was created.
+// var colors = [
+//     "rgb(255, 0, 0)",
+//     "rgb(255, 255, 0)",
+//     "rgb(0, 255, 0)",
+//     "rgb(0, 255, 255)",
+//     "rgb(0, 0, 255)",
+//     "rgb(255, 0, 255)",
+// ];
 
-   2. We then want to focus on coloring each box and using the random method.
-*/
-// console.log("sanity check")
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-];
-
-/* 
-Loop Through and assign a color to each square
-    - making use of working with the DOM
-*/
-
+// Now we define colors again, but now to randomly genenerate the number of colors to display
+var colors = generateRandomColors(6);   // we define colors and then we go and create the function(s)
 let squares = document.querySelectorAll(".square");
-console.log(squares);
-// let pickedColor = colors[3];
+// console.log(squares);
 let pickedColor = pickColor();  //pickColor() is the function that randomly assigns whatever the color will be
 let colorDisplay = document.getElementById("colorDisplay");
-
-// we create a variable called messageDisplay; it allows me to call on it later; will do when the user guesses wrong.
-let messageDisplay = document.querySelector("#message");
-
+let messageDisplay = document.querySelector("#message");// we create a variable called messageDisplay; it allows me to call on it later; will do when the user guesses wrong.
 colorDisplay.textContent = pickedColor;
 
-// add EventListeners 
-for (var i = 0; i < squares.length; i++){  
-    // adding the initial colors to squares
-    squares[i].style.backgroundColor = colors[i];
-    // adding an EventListener, an click listener to the squares
-    squares[i].addEventListener("click", function (){ 
-    // alert("clicked a square"); 
-    // Select the color of the clicked square; ('THIS' refers to the item we clicked on, the actual square.)
-    // alert(this.style.backgroundColor);  // Since I am using, "this", I had to change my 'arrow' function to standard anonoyomous function
-        var clickedColor=this.style.backgroundColor;
-        // Then compare it to the pickedColor through an if statement
-        if(clickedColor === pickedColor){
-        messageDisplay.textContent = "Correct. Excellent Job!";
-        changeColors(clickedColor);
+
+// ADD EVENT LISTENERS
+for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i]; // adding the initial colors to squares
+    squares[i].addEventListener("click", function () { // adding an EventListener, an click listener to the squares
+        // alert("clicked a square"); 
+        var clickedColor = this.style.backgroundColor; // Select the color of the clicked square; ('THIS' refers to the item we clicked on, the actual square.)
+        console.log(clickedColor, pickedColor);
+        if (clickedColor === pickedColor) { // Compare the color clicked to the picked Color to guess
+            messageDisplay.textContent = "Correct. Excellent Job!";
+            changeColors(clickedColor);
         } else {
-            // if the user clicks one of the wrong ones, the color will fade into the background, thus disappearing. 
-            this.style.backgroundColor = "#151515";
-            // and the user will show a message in the <span> tag 
-            messageDisplay.textContent = "Don't give up! Try Again";
+            this.style.backgroundColor = "#151515"; // if the user clicks one of the wrong ones, the color will fade into the background, thus disappearing. 
+            messageDisplay.textContent = "Don't give up! Try Again"; // and show the user a message in a <span> tag 
         }
     });
-    // console.dir(squares[i]);
-    // console.log(colors[i]);
 }
 
-
-// Now, once the player selects the correct color, we need to loop through all squares and change each clor to match the given color
-function changeColors(color){
-//loop through squares:
-for(var i = 0; i < squares.length; i++){
-    //now change each color to match given color
-    squares[i].style.backgroundColor = color;
-    // AFTER creating this function, we then have to call on it above when the user clicks the Correct, pickedColor.
+function changeColors(color) {// once the player selects the correct color, we need to loop through all squares and change each clor to match the given color
+    for (var i = 0; i < squares.length; i++) { //loop through squares:
+        squares[i].style.backgroundColor = color; // change each square, 'i' to match given color
     }
 }
-function pickColor(){
-    // first, randomly select a color from our array and assign it to a varaiable;
-    var random = Math.floor(Math.random() * colors.length);
-    // now, we return the number associated with the colors array that was returned.
-    return colors[random];
-    // return colorsArray[and give us this random number that identifies the color in the colorsArray]
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length); //randomly select a color from our array and assign it to the color to guess;
+    return colors[random]; // return colorsArray and give us this random number that identifies the color in the colorsArray
 }
+
+function generateRandomColors(num) { //this will return only ONE color so we need to create another function to call on it mult times
+    var arr = [];
+    for (i = 0; i < num; i++) {
+        arr.push(randomColor()); // push the randomColor in the array and continue the loop
+    }
+    return arr;
+};
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + "" + ", " + b + ")";
+};
